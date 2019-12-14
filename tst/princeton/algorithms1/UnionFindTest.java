d99953e541346d94a3939c684db06c9d50b285c4
tom <tom.drozdowski@gmail.com>
2019-12-14 07:00:22 -0600

    Finished first basic test for Union Find

--- tst/princeton/algorithms1/UnionFindTest.java
+++ tst/princeton/algorithms1/UnionFindTest.java
@@ -1,9 +1,13 @@
 package princeton.algorithms1;
 
 import com.google.common.collect.ImmutableList;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class UnionFindTest {
 
@@ -11,6 +15,10 @@ public class UnionFindTest {
     public void basicTest() {
         UnionFind uf = new UnionFind(10);
         getUnions().forEach(ints -> uf.union(ints[0], ints[1]));
+
+        assertTrue(uf.connected(4, 3));
+        assertTrue(uf.connected(3, 9));
+        assertFalse(uf.connected(1, 9));
     }
 
     List<int[]> getUnions() {
