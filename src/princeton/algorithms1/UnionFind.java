d3898541f1f1a9296ff1a49b15691f021182dac2
Tom <tom.drozdowski@gmail.com>
2019-12-13 22:43:06 -0600

    Created some shell classes

--- src/princeton/algorithms1/UnionFind.java
+++ src/princeton/algorithms1/UnionFind.java
@@ -1,5 +1,19 @@
 package princeton.algorithms1;
 
-public class UnionFind {
+import lombok.Builder;
 
+@Builder
+public class UnionFind {
+    private int n;
+        public UnionFind(int n) {
+            this.n = n;
+        }
+
+        public void union(int p, int q) {
+
+        }
+
+        public boolean connected(int p, int q) {
+            return false;
+        }
 }
--- tst/princeton/algorithms1/UnionFindTest.java
+++ tst/princeton/algorithms1/UnionFindTest.java
@@ -1,4 +1,31 @@
 package princeton.algorithms1;
 
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
+import java.util.List;
+
 public class UnionFindTest {
+
+    @Test
+    public void basicTest() {
+        UnionFind uf = new UnionFind(10);
+        getUnions().forEach(ints -> uf.union(ints[0], ints[1]));
+    }
+
+    List<int[]> getUnions() {
+        return ImmutableList.of(
+                new int[] {4, 3},
+                new int[] {3, 8},
+                new int[] {6, 5},
+                new int[] {9, 4},
+                new int[] {2, 1},
+                new int[] {8, 9},
+                new int[] {5, 0},
+                new int[] {7, 2},
+                new int[] {6, 1},
+                new int[] {1, 0},
+                new int[] {6, 7}
+        );
+    }
 }
